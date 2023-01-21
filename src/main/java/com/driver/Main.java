76a84852959208e0ece94235cb42fde7064a0ea3
vamsipa1 <vamsipa1311@gmail.com>
2023-01-21 18:53:20 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+   public int product(int x,int y){
+    int ans=x*y;
+    return ans;
+ }
+   public int product(int x,int y,int z){
+            int value=x*y*z;
+            return value;
+        }
+        public double product(double x,double y){
+            double value=x*y;
+            return value;
+        }
+    }
+    public static void main(String[] args) {
+        Product p=new Product();
+        int firstValue=p.product(2,3);
+        int secondValue=p.product(2,3,7);
+        double thirdValue=p.product(2.7,3.0);
+    }
 
 }
